db58c4065f65649f0c3fc47418096d495029e5eb
Ademola Kolawole <kademola@Wales-MBP.home>
2020-09-18 02:07:30 -0400

    alert winner in script

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,11 @@ let oneNumShot = 0,
   twoNumGoal = 0,
   chance = 60; // Declare and assign a value of 60 to determine if a shot is succedd or fail
 
+// Audio for goal and missed shot
+let goalAudio = document.querySelector("#goal-audio");
+let missAudio = document.querySelector("#missed-audio");
+
+// Team 1 shot and miss attempted
 let teamOneButton = document.querySelector("#teamone-shoot-button");
 teamOneButton.addEventListener("click", function () {
   oneNumShot += 1; // Increase Team 1 Number shot by 1
@@ -16,9 +21,13 @@ teamOneButton.addEventListener("click", function () {
   if (radNum > chance) {
     oneNumGoal += 1; // Increase Team 1 Number Goal by 1 when random is greater than chance value of 60
     teamOneGoal.innerHTML = oneNumGoal;
+    goalAudio.play(); //Play sound when goal shot
+  } else {
+    missAudio.play(); //Play sound when missed shot
   }
 });
 
+// Team 2 shot and miss attempted
 let teamTwoButton = document.querySelector("#teamtwo-shoot-button");
 teamTwoButton.addEventListener("click", function () {
   twoNumShot += 1; // Increase Team 2 Number Shot by 1
@@ -27,13 +36,27 @@ teamTwoButton.addEventListener("click", function () {
   if (radNum > chance) {
     twoNumGoal += 1; // Increase Team 2 Number Goal by 1 when random is greater than chance value of 60
     teamTwoGoal.innerHTML = twoNumGoal;
+    goalAudio.play(); //Play sound when goal shot
+  } else {
+    missAudio.play(); //Play sound when missed shot
   }
 });
 
+// Reset and add counter to button
 let reset = 0;
 let resetNum = document.querySelector("#num-resets");
 let resetButton = document.querySelector("#reset-button");
 resetButton.addEventListener("click", function () {
+  // Check who the winner is between Team 1 and Team 2
+  if (twoNumGoal > oneNumGoal) {
+    alert("Team 2 is the Winner");
+  } else if (twoNumGoal < oneNumGoal) {
+    alert("Team 1 is the Winner");
+  } else {
+    alert("It's a Draw");
+  }
+
+  // Reset the shot taken and goals
   teamOneGoal.innerHTML = 0;
   teamOneNumShot.innerHTML = 0;
   teamTwoGoal.innerHTML = 0;
