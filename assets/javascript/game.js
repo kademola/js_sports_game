e6541933590443efdbc1f937001cddc66492d228
Ademola Kolawole <kademola@Wales-MBP.home>
2020-09-17 23:30:05 -0400

    sound

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,26 +6,26 @@ let oneNumShot = 0,
   twoNumShot = 0,
   oneNumGoal = 0,
   twoNumGoal = 0,
-  chance = 60;
+  chance = 60; // Declare and assign a value of 60 to determine if a shot is succedd or fail
 
 let teamOneButton = document.querySelector("#teamone-shoot-button");
 teamOneButton.addEventListener("click", function () {
-  oneNumShot += 1;
+  oneNumShot += 1; // Increase Team 1 Number shot by 1
   teamOneNumShot.innerHTML = oneNumShot;
-  let radNum = Math.floor(Math.random() * 101);
+  let radNum = Math.floor(Math.random() * 101); // Random number from 0 to 100
   if (radNum > chance) {
-    oneNumGoal += 1;
+    oneNumGoal += 1; // Increase Team 1 Number Goal by 1 when random is greater than chance value of 60
     teamOneGoal.innerHTML = oneNumGoal;
   }
 });
 
 let teamTwoButton = document.querySelector("#teamtwo-shoot-button");
 teamTwoButton.addEventListener("click", function () {
-  twoNumShot += 1;
+  twoNumShot += 1; // Increase Team 2 Number Shot by 1
   teamTwoNumShot.innerHTML = twoNumShot;
-  let radNum = Math.floor(Math.random() * 101);
+  let radNum = Math.floor(Math.random() * 101); // Random number from 0 to 100
   if (radNum > chance) {
-    twoNumGoal += 1;
+    twoNumGoal += 1; // Increase Team 2 Number Goal by 1 when random is greater than chance value of 60
     teamTwoGoal.innerHTML = twoNumGoal;
   }
 });
@@ -34,11 +34,14 @@ let reset = 0;
 let resetNum = document.querySelector("#num-resets");
 let resetButton = document.querySelector("#reset-button");
 resetButton.addEventListener("click", function () {
-  //   location.reload();
   teamOneGoal.innerHTML = 0;
   teamOneNumShot.innerHTML = 0;
   teamTwoGoal.innerHTML = 0;
   teamTwoNumShot.innerHTML = 0;
   reset += 1;
   resetNum.innerHTML = reset;
+  oneNumShot = 0;
+  oneNumGoal = 0;
+  twoNumShot = 0;
+  twoNumGoal = 0;
 });
